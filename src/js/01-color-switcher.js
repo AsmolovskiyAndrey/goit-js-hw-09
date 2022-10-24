81c01cd43c1b07e99d86a02ec8ae33e5d42c979a
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2022-10-24 20:15:24 +0200

    1+

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+stopBtn.disabled = true; //!кнопка стоп изначально неактивна
+let timerId = null;
+
+const clickStartBtn = startBtn.addEventListener('click', onStartClick);
+const clickStopBtn = stopBtn.addEventListener('click', onStopClick);
+
+
+function onStartClick() {
+    changeColorBGC(); //! запустит в первый раз изменение BGC
+    timerId = setInterval(changeColorBGC, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+}
+function onStopClick() {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+}
+
+function changeColorBGC() {
+    body.style.backgroundColor = getRandomHexColor();
+}
